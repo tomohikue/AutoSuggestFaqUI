66982ef1cbb00438e46695bac30e7df98325ac26
pankun8g <pankun8g@gmail.com>
2021-05-22 19:59:26 +0900

    json更新
    json更新

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 
 // QnA Makerから取得する情報
-export const QnAAuthKey = 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXX';
-export const QnAEndpointURL = 'https://XXXX.azurewebsites.net/qnamaker/knowledgebases/XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXX/generateAnswer';
+export const QnAAuthKey = '3bc153db-1037-480b-929e-ddea941bb51a';
+export const QnAEndpointURL = 'https://qnacookaitest3.azurewebsites.net/qnamaker/knowledgebases/55219a49-6d97-48fc-bd7a-10b9e581102d/generateAnswer';
 export const QnAGetCnt = '5';
 
